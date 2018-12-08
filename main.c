3978bbe2ff3fde9640d1fbf49a47c97da3351ba4
Noldor123 <37693340+Noldor123@users.noreply.github.com>
2018-12-08 12:02:40 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(void) {
@@ -30,5 +31,6 @@ int main(void) {
     printf("%s\n", newstr);
     memset(&newstr, 0, strSize);
     shift = 0;
+    free(str);
   }
 }
